d8417ffcd1a41b221e7c3a5c131d017c6733c586
Daniel Trevisani <danil85@gmail.com>
2025-10-17 12:33:10 +0000

    Concatenação de Dados

---
+++ concat_dados.py
@@ -0,0 +1,8 @@
+#Vamos receber dois dados diferentes dos usuários e, depois disso, concatená-los.
+
+info1 = input("Digite a primeira informação:")
+info2 = input("Digite a segunda informação:")
+
+info_concatenada = info1 + " " + info2
+
+print("As informações concatenadas são: ", info_concatenada)
