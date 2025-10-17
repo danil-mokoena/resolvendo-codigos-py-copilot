c50e7b70d872cac6f2cfd5776ed5919738a95330
Daniel Trevisani <danil85@gmail.com>
2025-10-17 12:46:49 +0000

    Finalizando o arquivo de concatenação

--- concat_dados.py
+++ concat_dados.py
@@ -6,3 +6,6 @@ info2 = input("Digite a segunda informação:")
 info_concatenada = info1 + " " + info2
 
 print("As informações concatenadas são: ", info_concatenada)
+
+#Também poderíamos utilizar uma versão mais moderna e direta para alcançar o mesmo objetivo
+print (f"As informações concatenadas são: {info1} {info2}")
