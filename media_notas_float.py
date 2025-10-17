5e0d1118975d818ad0314bd60f4cecca4d84b300
Daniel Trevisani <danil85@gmail.com>
2025-10-17 13:44:45 +0000

    Arquivo para calcular a média de três notas utilizando float

---
+++ media_notas_float.py
@@ -0,0 +1,12 @@
+# Programa para calcular a média de três notas
+
+# Entrada de dados:
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+# Cálculo da média:
+media = (nota1 + nota2 + nota3) / 3
+
+# Saída (resultado):
+print("A média das notas é:", media)
