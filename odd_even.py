e35669adaf9b8423e973fcf0cc1fea8af1288f2c
Daniel Trevisani <danil85@gmail.com>
2025-10-17 13:42:54 +0000

    Arquivo para verificação se um número é par ou ímpar

---
+++ odd_even.py
@@ -0,0 +1,10 @@
+# O programa verifica se um número é par ou ímpar
+
+# Entrada de dados:
+numero = int(input("Digite um número inteiro: "))
+
+# Estrutura condicional:
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
